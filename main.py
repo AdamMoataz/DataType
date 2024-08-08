b89de4e9d68cb80a25cd43282ae242a23902b7ec
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-08-08 21:21:44 +0300

    i learne dall the data types

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 # Data Type
 
 # string 
-name = "Mohammed Musab Khan"
+name = "Mohammed Musab Khan" 
+burger = "2"
+biryani = "3 plates"
+print(type(name))
+
+# int
+burger = 2
+biryani = 3
+age = 10
+
+# float
+biryani = 2.50 
+exam_mark = 86.67
+phone_percentage = 48.42
+
+# list - list is editible 
+grocery_store = ["banana","apples","chesse","mango"]
+
+# tuple - tuble is not editible
+super_market = ("potato","brinjal","spring onion"," broccoli")
+
+# dictionary - key value
+contact_info = {"name":"Adam","Favorite_teacher":"Mr Musab Khan"}
+
+# boolean
+is_Summer_holiday = True
+School_day = False
+Coding_day = True
